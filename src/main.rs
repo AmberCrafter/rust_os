7797ad659da1bd2bdc1367d5042ce530bc430a9b
LeisurelyAzure <39610187+AmberCrafter@users.noreply.github.com>
2022-07-31 02:03:16 +0000

    finished vga text mode

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+use core::fmt::Write;
 
 mod lib;
 use lib::vga_buffer;
@@ -10,12 +11,15 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    println!("hello macro{}", " !");
     vga_buffer::test_hello_word();
+    vga_buffer::WRITER.lock().write_str("test message!").unwrap();
 
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
